5965660cf405ed908c0af0ed91a5ccf35a021707
Wilson Soetomo <wilsonsoetomo@wilsons-mbp.lan>
2022-05-21 15:12:10 -0700

    working readme generator

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# READmeGenerator
+
+# Wilson's READme Generator
+
+# Table of contents:
+- [Description](#description)
+- [Installation](#install)
+- [Usage](#usage)
+- [Credits](#credits)
+- [Instructions](#instructions)
+- [Github](#github)
+- [Email](#email)
+
+## Description:
+
+This project is going to give the user several questions and take the input for the READme
+## Installation:
+
+in the file director run node index
+## Usage:
+
+this project can be used to simplify the way a readme is made by quickly formatting it with questions.
+## Credits:
+
+The following project has been done by: undefined
+## Instructions:
+
+enter node index in the terminal and answer the questions accordingly.
+## Github:
+
+-[View the Github profile here](https://github.com/wilson148)
+## Email:
+
+for additional questions you can email me at soetomowilson@gmail.com.
+
+
+
+
+    
---
+++ index.js
@@ -0,0 +1,86 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+inquirer.prompt([{
+    type: 'input',
+    message: 'What is the title of the project?',
+    name: 'title',
+},
+{
+    type: 'input',
+    message: 'Please enter a description for the project.',
+    name: 'description',
+},
+{
+    type: 'input',
+    message: 'What are the installation instructions for this project?',
+    name: 'install',
+},
+{
+    type: 'input',
+    message: 'In what ways can this project be used?',
+    name: 'usage',
+},
+{
+    type: 'input',
+    message: 'Who contributed to creating this project?',
+    name: 'credits',
+},
+{
+    type: 'input',
+    message: 'What are the test instructions',
+    name: 'instructions',
+},
+{
+    type: 'input',
+    message: 'What is your Github username?',
+    name: 'github',
+},
+{
+    type: 'input',
+    message: 'What is your email?',
+    name: 'email',
+},
+])
+.then((response) => {
+    fs.writeFile("README.md",`
+# ${response.title}
+
+# Table of contents:
+- [Description](#description)
+- [Installation](#install)
+- [Usage](#usage)
+- [Credits](#credits)
+- [Instructions](#instructions)
+- [Github](#github)
+- [Email](#email)
+
+## Description:
+
+${response.description}
+## Installation:
+
+${response.install}
+## Usage:
+
+${response.usage}
+## Credits:
+
+The following project has been done by: ${response.credits}
+## Instructions:
+
+${response.instructions}.
+## Github:
+
+-[View the Github profile here](https://github.com/${response.github})
+## Email:
+
+for additional questions you can email me at ${response.email}.
+
+
+
+
+    `,(err) => console.log("error displaying"))
+    console.log(response)
+})
+
